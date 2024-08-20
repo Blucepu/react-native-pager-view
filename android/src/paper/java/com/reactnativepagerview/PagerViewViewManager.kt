a7df353443965064773e964cc12e4148330ed595
Blucepu <2287892008@qq.com>
2024-08-20 16:10:10 +0800

    "test"

--- android/src/paper/java/com/reactnativepagerview/PagerViewViewManager.kt
+++ android/src/paper/java/com/reactnativepagerview/PagerViewViewManager.kt
@@ -68,7 +68,7 @@ class PagerViewViewManager : ViewGroupManager<NestedScrollableHost>() {
         }
         host.addView(vp)
         val view = PagerViewViewManagerImpl.getViewPager(host)
-        view.reduceDragSensitivity(8)
+        view.reduceDragSensitivity(2)
         return host
     }
 
